1f29b9cde2479094713fdbaed6806cdb7b78bf38
vandej26 <joris.vandenbossche@infofarm.be>
2017-04-29 18:34:53 +0200

    Opslaan leningen

--- server.R
+++ server.R
@@ -13,13 +13,16 @@ shinyServer(function(input, output, session) {
     toggle(id = "lenVermdiv", condition = input$vermogenCheck)
   })
   
-  leningSamenstelling <- NULL
+  leningSamenstelling <- data.table(Te_Lenen_Bedrag = numeric(),
+                                    Vast_Of_Variabel = character(),
+                                    Rentevoet = numeric(),
+                                    Jaar = numeric())
   observeEvent(input$lenVoegToe, {
     # Invoer checken:
     checks <- TRUE
     
-    inputs <- c("lenTot", "lenRV", "lenJaar")
-    errorDivs <- c("lenTotError", "lenRVError", "lenJaarError")
+    inputs <- c("lenBedr", "lenRV", "lenJaar")
+    errorDivs <- c("lenBedrError", "lenRVError", "lenJaarError")
     
     for(inp in inputs){
       dum <- 
@@ -52,18 +55,23 @@ shinyServer(function(input, output, session) {
     leningSamenstelling <<- 
       rbind(leningSamenstelling, 
             data.table(
-              "Te_Lenen_Bedrag" = floor(lenTot),
+              "Te_Lenen_Bedrag" = floor(lenBedr),
               "Vast_Of_Variabel" = input$lenVastOfVar,
-              "rentevoet" = lenRV,
+              "Rentevoet" = lenRV,
               "Jaar" = floor(lenJaar)
             ))
+    toonLeningSamenstelling()
+  })
+  
+  toonLeningSamenstelling <- function(){
     output$lenInputDT <- renderDataTable(leningSamenstelling,
                                          options=list(autoWidth = TRUE,
                                                       scrollX=TRUE, 
                                                       paging = FALSE,
                                                       searching = FALSE,
                                                       pageLength = -1))
-  })
+  }
+  toonLeningSamenstelling()
   
   observeEvent(input$lenLaatsteWeg, {
     if(is.null(leningSamenstelling) || dim(leningSamenstelling)[1] == 0)
@@ -71,21 +79,143 @@ shinyServer(function(input, output, session) {
     
     leningSamenstelling <<- 
       leningSamenstelling[-dim(leningSamenstelling)[1]]
-    output$lenInputDT <- renderDataTable(leningSamenstelling,
-                                         options=list(autoWidth = TRUE,
-                                                      scrollX=TRUE, 
-                                                      paging = FALSE,
-                                                      searching = FALSE,
-                                                      pageLength = -1))
+    toonLeningSamenstelling()
+  })
+  
+  observeEvent(input$lenAllesWeg,{
+    leningSamenstelling <<- leningSamenstelling[-(1:dim(leningSamenstelling)[1])]
+    toonLeningSamenstelling()
   })
   
   # Leningen opslaan
-  # lenOpslaan knop
+  opgeslagenLeningen <- NULL
+  if(file.exists("opgeslagenLeningenVoorbeelden.RData"))
+    load(file = "opgeslagenLeningenVoorbeelden.RData")
+  
+  observeEvent(input$lenOpslaan, {
+    # Invoer checken:
+    checks <- TRUE
+    
+    if(is.null(leningSamenstelling) || dim(leningSamenstelling)[1] == 0){
+      toggle(id = "lenSamError", 
+             condition = TRUE)
+      checks <- FALSE
+    } else {
+      toggle(id = "lenSamError", 
+             condition = FALSE)
+    }
+    if(is.null(input$lenBank) || input$lenBank == ""){
+      toggle(id = "lenBankError", 
+             condition = TRUE)
+      checks <- FALSE
+    } else {
+      toggle(id = "lenBankError", 
+             condition = FALSE)
+    }
+    
+    inputs <- c()
+    errorDivs <- c()
+    if(input$kostenCheck){
+      inputs <- c(inputs, "lenKost1", "lenKostM", "lenKostJ")
+      errorDivs <- c(errorDivs, "lenKost1Error", "lenKostMError", "lenKostJError")
+    } else {
+      lenKost1 <- NA
+      lenKostM <- NA
+      lenKostJ <- NA
+    }
+    if(input$inflCheck){
+      inputs <- c(inputs, "lenInfl")
+      errorDivs <- c(errorDivs, "lenInflError")
+    } else {
+      lenInfl <- NA
+    }
+    if(input$vermogenCheck){
+      inputs <- c(inputs, "lenVermStart", "lenVermInk", "lenVermUit", 
+                  "lenVermBelPerc", "lenVermBelOpbrPerc")
+      errorDivs <- c(errorDivs, "lenVermStartError", "lenVermInkError", 
+                     "lenVermUitError", "lenVermBelPercError", "lenVermBelOpbrPercError")
+    } else {
+      lenVermStart <- NA
+      lenVermInk <- NA
+      lenVermUit <- NA
+      lenVermBelPerc <- NA
+      lenVermBelOpbrPerc <- NA
+    }
+    
+    for(inp in inputs){
+      dum <- 
+        tryCatch({
+          as.numeric(
+            gsub(",", "\\.", input[[inp]])
+          )
+        },
+        warning = function(x){
+          NA
+        })
+      
+      if(is.na(dum)){
+        toggle(id = errorDivs[which(inputs == inp)], 
+               condition = TRUE)
+        print(paste("Error: ", inp))
+        checks <- FALSE
+      } else {
+        toggle(id = errorDivs[which(inputs == inp)], 
+               condition = FALSE)
+        assign(x = inp, 
+               value = dum)
+      }
+    }
+    
+    if(!checks)
+      return(NULL)
+    # Opslaan:
+    opgeslagenLeningen <<- rbind(opgeslagenLeningen, data.table(
+      "Bank" = input$lenBank,
+      "Te_Lenen_Bedrag" = paste(leningSamenstelling$Te_Lenen_Bedrag, collapse = "/"),
+      "Vast_Of_Variabel" = paste(leningSamenstelling$Vast_Of_Variabel, collapse = "/"),
+      "Rentevoet" = paste(leningSamenstelling$Rentevoet, collapse = "/"),
+      "Jaar" = paste(leningSamenstelling$Jaar, collapse = "/"),
+      "Kosten_Bijhouden" = input$kostenCheck,
+      "Kosten_Eenmalig" = lenKost1,
+      "Kosten_Maandelijks" = lenKostM,
+      "Kosten_Jaarlijks" = lenKostJ,
+      "Inflatie_Inrekenen" = input$inflCheck,
+      "Inflatie_Percentage" = lenInfl,
+      "Vermogen_Bijhouden" = input$vermogenCheck,
+      "Vermogen_Start" = lenVermStart,
+      "Vermogen_Maandelijske_Inkomsten" = lenVermInk,
+      "Vermogen_Maandelijske_Uitgaven" = lenVermUit,
+      "Vermogen_Beleggingspercentage" = lenVermBelPerc,
+      "Vermogen_Opbrengst" = lenVermBelOpbrPerc
+    ))
+    
+    toonOpgeslagenLeningen()
+  })
+  
+  toonOpgeslagenLeningen <- function(){
+    if(is.null(opgeslagenLeningen) || dim(opgeslagenLeningen)[1] == 0){
+      output$leningenDT <- 
+        renderDataTable(data.table(Lening ="Geen opgeslagen leningen gevonden!"))
+    } else {
+      output$leningenDT <- renderDataTable(opgeslagenLeningen,
+                                           options=list(autoWidth = TRUE,
+                                                        scrollX=TRUE, 
+                                                        paging = FALSE,
+                                                        searching = FALSE,
+                                                        pageLength = -1))
+    }
+  }
+  toonOpgeslagenLeningen()
+  
   
   # Lening berekenen
   # lenBereken knop
   
   # Toggle off all errorDivs
-  for(errorDiv in c("lenTotError", "lenRVError", "lenJaarError"))
+  for(errorDiv in c("lenBedrError", "lenRVError", "lenJaarError",
+                    "lenSamError", "lenBankError",
+                    "lenKost1Error", "lenKostMError", "lenKostJError", "lenInflError",
+                    "lenVermStartError", "lenVermInkError", "lenVermUitError", "lenVermBelPercError",
+                    "lenVermBelOpbrPercError"))
     toggle(id = errorDiv, condition = FALSE)
 })
--- ui.R
+++ ui.R
@@ -37,10 +37,10 @@ body <- dashboardBody(
               fluidRow(
                 column(
                   width = 6,
-                  textInput("lenTot",
+                  textInput("lenBedr",
                             "Te lenen bedrag in euro:",
                             placeholder = "150000"),
-                  div(id = "lenTotError",
+                  div(id = "lenBedrError",
                       em(HTML("<font color='red'>Gelieve een juist getal in te geven.</font>")),
                       br()),
                   br(),
@@ -57,7 +57,7 @@ body <- dashboardBody(
                             "Jaar:",
                             placeholder = "25"),
                   div(id = "lenJaarError",
-                      p(em(HTML("<font color='red'> Gelieve een juist getal in te geven.</font>")))),
+                      p(em(HTML("<font color='red'>Gelieve een juist getal in te geven.</font>")))),
                   br(),
                   actionButton(
                     "lenVoegToe",
@@ -65,11 +65,16 @@ body <- dashboardBody(
                   actionButton(
                     "lenLaatsteWeg",
                     "Verwijder laatse invoer"),
+                  actionButton(
+                    "lenAllesWeg",
+                    "Verwijder alles"),
                   br(),
                   br(),
                   br(),
                   br(),
-                  dataTableOutput("lenInputDT")
+                  dataTableOutput("lenInputDT"),
+                  div(id = "lenSamError",
+                      p(em(HTML("<font color='red'>Gelieve een leninghoeveelheid in te geven.</font>"))))
                 ),
                 column(
                   width = 6,
@@ -83,14 +88,20 @@ body <- dashboardBody(
                     textInput("lenKost1",
                               "Eenmalige kosten, zoals bijvoorbeeld dossierkosten: ", 
                               placeholder = "500"),
+                    div(id = "lenKost1Error",
+                        p(em(HTML("<font color='red'>Gelieve een juist getalin te geven.</font>")))),
                     textInput("lenKostM", 
                               paste0("Maandlijke kosten, zoals bijvoorbeeld ",
                                      "bankrekeningkosten of schuldsaldo verzekering: "), 
                               placeholder = "162,62"),
+                    div(id = "lenKostMError",
+                        p(em(HTML("<font color='red'>Gelieve een juist getal in te geven.</font>")))),
                     textInput("lenKostJ", 
                               paste0("Jaarlijkse kosten, zoals bijvoorbeeld ",
                                      "brandverzekerning: "), 
                               placeholder = "256,3")),
+                  div(id = "lenKostJError",
+                      p(em(HTML("<font color='red'>Gelieve een juist getal in te geven.</font>")))),
                   checkboxInput(
                     "inflCheck",
                     "Inflatie", 
@@ -100,6 +111,8 @@ body <- dashboardBody(
                     textInput("lenInfl", 
                               "Inflatie in percent per jaar: ", 
                               placeholder = "2,0")),
+                  div(id = "lenInflError",
+                      p(em(HTML("<font color='red'>Gelieve een juist getal in te geven.</font>")))),
                   checkboxInput(
                     "vermogenCheck",
                     "Vermogen bijhouden", 
@@ -109,18 +122,29 @@ body <- dashboardBody(
                     textInput("lenVermStart", 
                               "Vermogen bij start ingang lening:", 
                               placeholder = "20000"),
+                    div(id = "lenVermStartError",
+                        p(em(HTML("<font color='red'>Gelieve een juist getal in te geven.</font>")))),
                     textInput("lenVermInk", 
                               "Inkomsten per maand:", 
                               placeholder = "2400"),
+                    div(id = "lenVermInkError",
+                        p(em(HTML("<font color='red'>Gelieve een juist getal in te geven.</font>")))),
                     textInput("lenVermUit", 
                               "Uitgaven per maand (de lening niet meegeteld):", 
                               placeholder = "1200"),
+                    div(id = "lenVermUitError",
+                        p(em(HTML("<font color='red'>Gelieve een juist getal in te geven.</font>")))),
                     textInput("lenVermBelPerc", 
                               "Percentage van vermogen in beleggingen:", 
+                              value = 0,
                               placeholder = "40"),
+                    div(id = "lenVermBelPercError",
+                        p(em(HTML("<font color='red'>Gelieve een juist getal in te geven.</font>")))),
                     textInput("lenVermBelOpbrPerc", 
                               "Opbrengstpercentage van belegd vermogen per jaar:", 
-                              placeholder = "2.0")
+                              placeholder = "2.0"),
+                    div(id = "lenVermBelOpbrPercError",
+                        p(em(HTML("<font color='red'>Gelieve een juist getal in te geven.</font>"))))
                     )
                 )
               )
@@ -142,6 +166,8 @@ body <- dashboardBody(
                 "Deze lening opslaan:",
                 textInput("lenBank",
                           "Naam van de bank:"),
+                div(id = "lenBankError",
+                    p(em(HTML("<font color='red'>Gelieve een naam in te voeren.</font>")))),
                 actionButton(
                   "lenOpslaan",
                   "Opslaan")
@@ -152,7 +178,18 @@ body <- dashboardBody(
           tabPanel(
             "Opgeslagen leningen",
             wellPanel(
-              "2"
+              dataTableOutput("leningenDT"),
+              column(
+                width = 8,
+                align="center",
+                br(),
+                br(),
+                actionButton(
+                  "lenBereken2",
+                  label = "       Start simulatie",
+                  icon("play"),
+                  style="color: #fff; background-color: #337ab7; border-color: #2e6da4")
+              )
             )
           )
         )
